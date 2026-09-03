117156e005d77a911c667bfcb022a1036dee3855
shrvaaan <shravanpawale19@gmail.com>
2026-09-03 18:02:45 +0530

    Adding lab15 work

---
+++ Lab15/question3.py
@@ -0,0 +1,17 @@
+#question3
+#sept 3, 2026
+
+k=input("enter first integer: ")
+n=input("enter second integr: ")
+k=int(k)
+n=int(n)
+print("arithmatic equations:" )1
+print(k*n)
+print(n-k)
+print(n*k)
+print(k*n)
+print(n/k)
+print(k//n)
+print(n**k)
+print(k%n)
+print(-n+k)
---
+++ Lab15/question4.py
@@ -0,0 +1,15 @@
+#Lab15
+# sept 3, 2026
+import math
+
+a= input("real number a: ")
+b= input("real number b: ")
+c= input("real number c: ")
+a=int(a)
+b=int(b)
+c=int(c)
+x= b**2 - 4*a*c
+root1 = (-b + math.sqrt(x))/(2*a)
+root2 = (-b - math.sqrt(x))/(2*a)
+
+print(root1,root2)
